8f155613809f1c89f9232712d987685c6b91346c
gcaliandro@gmail.com <gcaliandro@gmail.com>
2025-05-08 14:31:46 +0200

    feat: :sparkles: Updated prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,17 @@
-
 /**
  * @type {import('prettier').Config}
- * @type {import('prettier-plugin-tailwindcss').PluginOptions} 
+ * @type {import('prettier-plugin-tailwindcss').PluginOptions}
  */
 const prettierConfig = {
-	plugins: ["prettier-plugin-tailwindcss"],
-	bracketSameLine: true,
-	printWidth: 145,
-	useTabs: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 145,
+  quoteProps: "as-needed",
+  useTabs: false,
+  arrowParens: "always",
+  tabWidth: 2,
+  trailingComma: "es5",
 };
 
 export default prettierConfig;
